3259f75cb1708d865e542e87f9f996ffccbcf41c
= <kristoph321@gmail.com>
2017-04-03 13:08:06 +0200

    Naprawione ustawianie kolorow

--- ProgramGraficzny/Canvas.cpp
+++ ProgramGraficzny/Canvas.cpp
@@ -1,9 +1,15 @@
 #include "Canvas.h"
 #include <SDL.h>
 
+struct Colour {
+	Uint8 red;
+	Uint8 green;
+	Uint8 blue;
+};
+
 Canvas::Canvas(int width_of_canvas, int height_of_canvas)
 {
-	surface = SDL_CreateRGBSurface(0, width_of_canvas, height_of_canvas, 24,0,0,0,0);
+	surface = SDL_CreateRGBSurface(0, width_of_canvas, height_of_canvas, 24,0x00000FF,0x0000FF00,0x00FF0000,0);
 	SDL_Rect r = { 0, 0, width_of_canvas, height_of_canvas };
 	SDL_FillRect(surface, &r, WHITE);
 
@@ -13,19 +19,27 @@ Canvas::~Canvas() {
 	SDL_FreeSurface(surface);
 }
 
-void Canvas::set_pixel(Pixel pos, int color)
+void Canvas::set_pixel(Pixel pos, int colour)
 {
-	Uint8 *target_pixel = (Uint8 *)surface->pixels + pos.y * surface->pitch + pos.x*3;
-	*(int *)target_pixel = color;
+	SDL_LockSurface(surface);
+	Uint8 *target_pixel = (Uint8 *)surface->pixels + pos.y * surface->pitch + pos.x * 3;
+	Colour *target = (Colour *)target_pixel;
+	Colour our_colour;
+	our_colour.red  = (Uint8)(colour >> 16);
+	our_colour.green = (Uint8)(colour >> 8);
+	our_colour.blue = (Uint8)(colour >> 0);
+
+	*target = our_colour;
+	SDL_UnlockSurface(surface);
 }
-void Canvas::draw_line(Pixel P1, Pixel P2, int color) {
+void Canvas::draw_line(Pixel P1, Pixel P2, int colour) {
 	double x = P2.x - P1.x;
 	double y = P2.y - P1.y;
 	double length = sqrt(x*x + y*y);
 
 	if (length < 1)
 	{
-		set_pixel(P1, color);
+		set_pixel(P1, colour);
 		return; 
 
 	}
@@ -38,7 +52,7 @@ void Canvas::draw_line(Pixel P1, Pixel P2, int color) {
 
 	for (double i = 0; i < length; i += 1)
 	{
-		set_pixel(Pixel((int)x, (int)y),color);
+		set_pixel(Pixel((int)x, (int)y),colour);
 		x += addx;
 		y += addy;
 	}
